888858279cf53ad6cd07ebf1b7b939930920ed01
LordThanksalot <fransmertens1@gmail.com>
2018-02-04 10:23:44 +0100

    Reasoning1

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,11 @@ package main;
 
 public class App {
 	
-	
+	//optie: locaties at random kiezen, daar de som van nemen en zo totdat at random alle locatiecombinaties afgegaan zijn
+	//aantal mogelijke combinaties 3x3=6, 4x4=24, 5x5=120, 15x15=1.3 biljoen
+	//dit is teveel om volgens deze methode te kunnen toepassen
+	//MAAR: de vraag komt neer op een geheel aantal permutaties bvb 1,2,3 -> 1,2,3 - 1,3,2 - 2,1,3 - 2,3,1 - 3,1,2 - 3,2,1
+	//waarbij de index de rij is de waarde de kolom
 
 	public static void main(String[] args) {
 
