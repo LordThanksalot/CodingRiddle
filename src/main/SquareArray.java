b18df58607943513004e6bf52711a29385a7788d
LordThanksalot <fransmertens1@gmail.com>
2018-02-04 11:04:19 +0100

    Added solution example

--- src/main/SquareArray.java
+++ src/main/SquareArray.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SquareArray {
-	
-	//centrale locatie size
+
+	// centrale locatie size
 	private int size = 5;
 	private int[][] array2d = new int[size][size];
 	private int solutionSize = findNumberOfSolutions();
 	private int[] solutions = new int[solutionSize];
 	private int numberOfSolutions;
-	
-	//methode uit StackOverflow
+
+	// methode uit StackOverflow
 	public void permutingArray(List<Integer> arrayList, int element) {
 		for (int i = element; i < arrayList.size(); i++) {
 			java.util.Collections.swap(arrayList, i, element);
@@ -23,8 +23,8 @@ public class SquareArray {
 			System.out.println(Arrays.toString(arrayList.toArray()));
 		}
 	}
-	
-	//2 methodes verkregen uit StackOverflow en aangepast
+
+	// 2 methodes verkregen uit StackOverflow en aangepast
 	private int findNumberOfSolutions() {
 		return getFact(size) / (getFact(0));
 	}
@@ -37,5 +37,34 @@ public class SquareArray {
 		}
 
 		return f;
+	}
+
+	// voorbeeldoplossing met array size = 5
+	{
+		array2d[0][0] = 7;
+		array2d[0][1] = 53;
+		array2d[0][2] = 183;
+		array2d[0][3] = 439;
+		array2d[0][4] = 863;
+		array2d[1][0] = 497;
+		array2d[1][1] = 383;
+		array2d[1][2] = 563;
+		array2d[1][3] = 79;
+		array2d[1][4] = 973;
+		array2d[2][0] = 287;
+		array2d[2][1] = 63;
+		array2d[2][2] = 343;
+		array2d[2][3] = 169;
+		array2d[2][4] = 583;
+		array2d[3][0] = 627;
+		array2d[3][1] = 343;
+		array2d[3][2] = 773;
+		array2d[3][3] = 959;
+		array2d[3][4] = 943;
+		array2d[4][0] = 767;
+		array2d[4][1] = 473;
+		array2d[4][2] = 103;
+		array2d[4][3] = 699;
+		array2d[4][4] = 303;
 	}
 }
