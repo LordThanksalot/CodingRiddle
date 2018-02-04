18bf3b75d1e18225a65ebe9734a19944d40b9e65
LordThanksalot <fransmertens1@gmail.com>
2018-02-04 10:34:48 +0100

    First variables

--- src/main/App.java
+++ src/main/App.java
@@ -7,7 +7,9 @@ public class App {
 	//dit is teveel om volgens deze methode te kunnen toepassen
 	//MAAR: de vraag komt neer op een geheel aantal permutaties bvb 1,2,3 -> 1,2,3 - 1,3,2 - 2,1,3 - 2,3,1 - 3,1,2 - 3,2,1
 	//waarbij de index de rij is de waarde de kolom
-
+	//deze twee coördinaten stellen de positie voor in array2d om de waarde ervan te achterhalen, die op te tellen in een som die na
+	//het bereiken van de array2d size toegevoegd wordt aan een array van solutions
+	
 	public static void main(String[] args) {
 
 	}
--- src/main/SquareArray.java
+++ src/main/SquareArray.java
@@ -1,5 +1,12 @@
 package main;
 
 public class SquareArray {
-
+	
+	//centrale locatie size
+	private int size = 5;
+	private int[][] array2d = new int[size][size];
+	private int[] solutions = new int[Integer.MAX_VALUE];
+	private int numberOfSolutions;
+	
+	
 }
